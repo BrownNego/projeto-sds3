e261c47e349b08705ae6d0b9db6a9eaf7112c277
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-05-09 16:56:10 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/BrownNego" target="_blank" rel="noreferrer">Alex Oliveira</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/brown_nego" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/brown_nego" target="_blank" rel="noreferrer">@brown_nego</a></small></p>
             </div>
         </footer>
 
